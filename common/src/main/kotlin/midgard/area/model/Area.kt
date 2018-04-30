2db854975bd1c6079cd800a7af8e8c144b9ac7a9
fursovm <fursovm@amazon.com>
2018-04-30 08:28:57 -0400

    Simplified area model

--- common/src/main/kotlin/midgard/area/model/Area.kt
+++ common/src/main/kotlin/midgard/area/model/Area.kt
@@ -1,2 +1,10 @@
 package midgard.area.model
 
+
+enum class Direction { North, East, South, West, Up, Down }
+
+class Place(val exits: Map<Direction, Exit>)
+
+class Exit(val from: Place, val to: Place)
+
+
