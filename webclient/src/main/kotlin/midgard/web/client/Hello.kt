107d0a64de8fd26703d90a42012c0e87c4a7f4ee
Mikhail Fursov <fursovm@amazon.com>
2018-06-18 17:37:16 -0400

    Porting console.js to Kotlin

--- webclient/src/main/kotlin/midgard/web/client/Hello.kt
+++
@@ -1,6 +0,0 @@
-package midgard.web.client
-
-fun hello() {
-    console.log("Hello from `console.log()`!")
-    js("console('Hello')")
-}
---
+++ webclient/src/main/kotlin/midgard/web/client/JsConsole.kt
@@ -0,0 +1,77 @@
+import org.w3c.dom.*
+import org.w3c.dom.events.KeyboardEvent
+import kotlin.browser.document
+import kotlin.browser.window
+
+var socket: WebSocket? = null
+
+fun connect() {
+    console.log("Begin connect")
+    val s = WebSocket("ws://" + window.location.host + "/console")
+
+    s.onerror = { console.log("socket error") }
+
+    s.onopen = { write("Connected"); }
+
+    s.onclose = {
+        val e = it as CloseEvent
+        val explanation = when {
+            e.reason.isNotEmpty() -> "reason: ${e.reason}"
+            else -> "without a reason specified"
+        }
+        write("Disconnected with close code " + e.code + " and " + explanation)
+        window.setTimeout({ connect() }, 5000)
+    }
+
+    s.onmessage = { received((it as MessageEvent).data.toString()) }
+    socket = s
+}
+
+fun received(message: String) {
+    write(message)
+}
+
+fun write(message: String) {
+    val line = document.createElement("p") as HTMLElement
+    line.className = "message"
+    line.textContent = message
+
+    val messagesDiv = document.getElementById("messages") ?: return
+    messagesDiv.appendChild(line)
+    messagesDiv.scrollTop = line.offsetTop.toDouble()
+}
+
+fun onSend() {
+    val input = document.getElementById("commandInput") as? HTMLInputElement ?: return
+    val text = input.value
+    val s = socket
+    if (text.isNotEmpty() && s != null) {
+        s.send(text)
+        input.value = ""
+    }
+
+}
+
+fun start() {
+    connect()
+
+    val sendButton = document.getElementById("sendButton") as? HTMLElement ?: throw IllegalStateException("send button not found!")
+    sendButton.onclick = { onSend() }
+
+    val input = document.getElementById("commandInput") as? HTMLElement ?: throw IllegalStateException("input field not found!")
+    input.onkeydown = {
+        if ((it as KeyboardEvent).keyCode == 13) {
+            onSend()
+        }
+    }
+}
+
+@Suppress("unused")
+fun run() {
+    val sendButton = document.getElementById("sendButton")
+    if (sendButton != null) {
+        start()
+    } else {
+        window.setTimeout({ run() }, 300)
+    }
+}
